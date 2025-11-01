0fe821f1fcf15228efae2018ac8f0bc52c642ff0
adrilafosse <81637698+adrilafosse@users.noreply.github.com>
2025-11-01 15:19:46 +0100

    ok

---
+++ initialisationDB.js
@@ -0,0 +1,54 @@
+require('dotenv').config();
+const { Pool } = require('pg');
+
+const pool = new Pool({
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
+    ssl: { rejectUnauthorized: false }
+});
+
+async function initDB() {
+    try {
+        //await pool.query(`CREATE TABLE IF NOT EXISTS Metier (id_metier SERIAL PRIMARY KEY, nom text);`);
+        //await pool.query(`CREATE TABLE IF NOT EXISTS Utilisateur (id_utilisateur SERIAL PRIMARY KEY, pseudo TEXT, mot_de_passe TEXT, niveau INT, fk_metier int, foreign key(fk_metier) references Metier(id_metier));`);
+        //await pool.query(`CREATE TABLE IF NOT EXISTS Fabrication (id_fabrication SERIAL PRIMARY KEY, niveau INT, fk_metier int, fk_ressource int, foreign key(fk_metier) references Metier(id_metier), foreign key(fk_ressource) references Ressource(id_ressource));`);
+        //await pool.query(`CREATE TABLE IF NOT EXISTS Ressource (id_ressource SERIAL PRIMARY KEY, nom text);`);
+        //await pool.query(`CREATE TABLE IF NOT EXISTS Inventaire (id_inventaire SERIAL PRIMARY KEY, quantité int, fk_utilisateur int, fk_ressource int, foreign key(fk_utilisateur) references Utilisateur(id_utilisateur), foreign key (fk_ressource) references ressource(id_ressource));`);
+        //await pool.query(`Insert into Metier (nom) values('Alchimiste');`);
+        //await pool.query(`Insert into Metier (nom) values('Nain');`);
+        //await pool.query(`Insert into Fabrication (niveau, fk_metier, fk_ressource) values(3,1,6), (2,1,7), (1,1,8), (3, 2, 9), (2, 2, 10), (1,2,11), (1,2,12), (2,3,13), (3,3,14), (1,3,15);`);
+        //await pool.query(`Insert into Ressource (nom) values('bois'),('cristal de mana'), ('tissu'), ('acier'), ('pépite d''or'), ('bijoux'), ('armure'), ('pioche'), ('écailles de dragon'), ('minerais rares'), ('lingot d''or'), ('torche'), ('pierres précieuses') , ('armure magique'), ('potion')`);
+        //await pool.query(`Insert into Inventaire (quantité, fk_utilisateur, fk_ressource) values(100, 1, 1), (100, 1, 2), (100, 1, 3), (100, 1, 4), (100, 1, 5) ,(0, 1, 6),(0, 1, 7),(0, 1, 8),(0, 1, 9),(0, 1, 10),(0, 1, 11),(0, 1, 12),(0, 1, 13),(0, 1, 14),(0, 1, 15);`);
+        //await pool.query(`CREATE TABLE IF NOT EXISTS Cout_fabrication (id_cout_fabrication SERIAL PRIMARY KEY,quantite int,fk_fabrication int, fk_ressource int, foreign key(fk_fabrication) references Fabrication(id_fabrication), foreign key(fk_ressource) references Ressource(id_ressource));`);
+        //await pool.query(`Insert into cout_fabrication (quantite, fk_fabrication, fk_ressource) values(1 , 3, 1), (3, 3, 4), (1, 6, 4), (3, 6, 5), (1,10, 3), (3,10,2), (1,7,1), (1,7,2), (2,2,9), (1,2,3), (2,2,4), (2,5,8), (1,5,1),(1,5,2),(2,8,1),(1,8,10),(1,1,11),(1,1,13),(2,4,15),(1,4,14),(2,9,5),(2,9,6),(1,9,7);`);
+        //await pool.query(`CREATE TABLE IF NOT EXISTS Vente (id_vente SERIAL PRIMARY KEY, prix float, quantite int, statut boolean, fk_utilisateur int, fk_inventaire int, foreign key(fk_utilisateur) references utilisateur(id_utilisateur), foreign key (fk_inventaire) references inventaire(id_inventaire));`);
+        //await pool.query(`Drop table Ressource, Fabrication`);
+        //await pool.query(`DELETE FROM utilisateur`);
+        //await pool.query(`Insert into Ressource (nom) values('kamas')`);
+        //await pool.query(`update inventaire set quantité = 250 where fk_utilisateur = 3 and id_inventaire = 54`);
+        //await pool.query(`alter table vente add column fk_ressource int`);
+
+        console.log('✅ Base de données initialisée avec succès !');
+    } catch (err) {
+        console.error('❌ Erreur lors de l\'initialisation :', err.message);
+    } finally {
+        await pool.end();
+    }
+}
+
+//initDB();
+
+async function testDb() {
+    try {
+        const resultat = await pool.query(`select * from vente`);
+        console.log('✅ resultat =', resultat.rows);
+    } catch (err) {
+        console.error('❌ Erreur:', err.message);
+    } finally {
+        await pool.end();
+    }
+}
+testDb()
---
+++ server.js
@@ -0,0 +1,265 @@
+const express = require('express');
+const cors = require("cors");
+const { Pool } = require("pg");
+require('dotenv').config();
+const argon2 = require('argon2');
+
+const port = process.env.PORT || 5000
+
+const app = express();
+app.use(cors({
+    origin: 'https://concurrentpronote-production-e8c8.up.railway.app',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true
+}));
+app.use(express.json());
+
+const pool = new Pool({
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
+    ssl: { rejectUnauthorized: false }
+});
+
+//connexion
+app.post("/connexion", async(req, res) => {
+    const { pseudo, mot_de_passe } = req.body;
+    try {
+        const resultat = await pool.query("Select id_utilisateur,m.nom,u.fk_metier,u.niveau from utilisateur u join metier m on u.fk_metier =m.id_metier where pseudo = $1 and mot_de_passe=$2", [pseudo, mot_de_passe]);
+        if (resultat.rows.length > 0) {
+            //si connexion reussi
+            return res.status(200).json({ message: "connexion réussie", resultat: resultat.rows[0] });
+        } else {
+            //sinon mot de passe ou pseudo incorrect
+            return res.status(400).json({ message: "mot de passe ou pseudo incorrect" });
+        }
+    } catch (err) {
+        console.error(err)
+        res.status(500).send("Erreur connexion");
+    }
+});
+
+//inscription
+app.post("/inscription", async(req, res) => {
+    const { pseudo, mot_de_passe, id_metier } = req.body;
+    try {
+        const resultat = await pool.query("Select id_utilisateur from utilisateur where pseudo = $1", [pseudo]);
+        if (resultat.rows.lenght > 0) {
+            //si l'utilisateur existe déjà
+            return res.status(400).json({ message: "l'utilisateur existe déjà" });
+        } else {
+            //sinon on crée un nouvel utilisateur
+            const resultatSQL = await pool.query("Insert into utilisateur(pseudo, mot_de_passe,niveau, fk_metier) values ($1, $2,$3, $4) RETURNING id_utilisateur", [pseudo, mot_de_passe, 1, id_metier]);
+            await pool.query("Insert into Inventaire (quantité, fk_utilisateur, fk_ressource) values(10, $1, 1), (10, $1, 2), (10, $1, 3), (10, $1, 4), (10, $1, 5) ,(0, $1, 6),(0, $1, 7),(0, $1, 8),(0, $1, 9),(0, $1, 10),(0, $1, 11),(0, $1, 12),(0, $1, 13),(0, $1, 14),(0, $1, 15),(100,$1,16)", [resultatSQL.rows[0].id_utilisateur]);
+            res.status(201).json({ message: "utilisateur crée avec succès", resultat: resultatSQL.rows[0] });
+        }
+    } catch (err) {
+        console.error(err)
+        res.status(500).send("Erreur inscription");
+    }
+});
+
+//afficher fabrication possible
+app.post("/fabrication_possible", async(req, res) => {
+    const { fk_metier, niveau } = req.body;
+    try {
+        const resultat = await pool.query("Select f.id_fabrication, r.nom, f.id_fabrication from fabrication f join Ressource r on f.fk_ressource = r.id_ressource where f.niveau <= $1 and f.fk_metier = $2", [niveau, fk_metier]);
+        return res.status(200).json({ message: "fabrication possible récupéré avec succes", resultat: resultat.rows });
+    } catch (err) {
+        console.error(err)
+        res.status(500).send("Erreur fabrication_possible");
+    }
+});
+
+//Afficher cout fabrication d’un produit
+app.post("/cout_fabrication", async(req, res) => {
+    const { id_fabrication, id_utilisateur } = req.body;
+    try {
+        const resultat = await pool.query(" Select c.fk_fabrication, c.quantite, r.nom, r.id_ressource, i.quantité as quantité_possédé from cout_fabrication c join ressource r on c.fk_ressource = r.id_ressource join inventaire i on i.fk_ressource = r.id_ressource where c.fk_fabrication = $1 and i.fk_utilisateur =$2", [id_fabrication, id_utilisateur]);
+        return res.status(200).json({ message: "cout fabrication récupéré avec succes", resultat: resultat.rows });
+    } catch (err) {
+        console.error(err)
+        res.status(500).send("Erreur cout fabrication");
+    }
+});
+
+//Inventaire
+app.post("/inventaire", async(req, res) => {
+    const { id_utilisateur } = req.body;
+    try {
+        const resultat = await pool.query(" Select i.fk_ressource, r.nom, i.quantité, i.id_inventaire from inventaire i join ressource r on  i.fk_ressource = r.id_ressource where fk_utilisateur = $1 ", [id_utilisateur]);
+        return res.status(200).json({ message: "inventaire récupéré avec succes", resultat: resultat.rows });
+    } catch (err) {
+        console.error(err)
+        res.status(500).send("Erreur inventaire");
+    }
+});
+
+//fabrication un produit
+app.post("/fabrication", async(req, res) => {
+    const { id_utilisateur, id_fabrication } = req.body;
+    try {
+        //Recuperation des ressources necessaires et de l'inventaire
+        const resultat = await pool.query("Select c.fk_fabrication,f.fk_ressource as ressource_a_produire, r2.nom as produit, c.quantite as quantite_requis, r.nom ,i.id_inventaire, i.quantité as quantite_possede, i.fk_ressource from cout_fabrication c join ressource r on c.fk_ressource = r.id_ressource join inventaire i on i.fk_ressource = r.id_ressource join fabrication f on f.id_fabrication = c.fk_fabrication join ressource r2 on f.fk_ressource= r2.id_ressource where i.fk_utilisateur = $1 and f.id_fabrication = $2", [id_utilisateur, id_fabrication])
+        let bool = true;
+        //verification qu'on possede assez de ressources
+        for (let i = 0; i < resultat.rows.length; i++) {
+            if (resultat.rows[i].quantite_requis <= resultat.rows[i].quantite_possede && bool == true) {
+                bool = true;
+            } else {
+                bool = false;
+            }
+        }
+        if (bool == true) {
+            //est ce que on possede deja cette ressource ou pas 
+            const resultat2 = await pool.query("Select quantité, id_inventaire from inventaire where fk_utilisateur = $1 and fk_ressource = $2", [id_utilisateur, resultat.rows[0].ressource_a_produire]);
+            await pool.query("update inventaire set quantité=$1 where id_inventaire=$2", [resultat2.rows[0].quantité + 1, resultat2.rows[0].id_inventaire]);
+            for (let i = 0; i < resultat.rows.length; i++) {
+                //retier les ressources necessaire dans l'inventaire
+                await pool.query("update inventaire set quantité=$1 where id_inventaire=$2", [resultat.rows[i].quantite_possede - resultat.rows[i].quantite_requis, resultat.rows[i].id_inventaire]);
+            }
+            return res.status(201).json({ message: "ajout +1 dans l'inventaire" });
+        } else {
+            return res.status(400).json({ message: "vous n'avez pas les ressources nécessaire" });
+        }
+
+    } catch (err) {
+        console.error(err)
+        res.status(500).send("Erreur fabrication");
+    }
+});
+
+//augmenter niveau
+app.post("/augmenter_niveau", async(req, res) => {
+    const { id_utilisateur } = req.body;
+    try {
+        //verifier que l'utilisateur a assez de kamas
+        const resultat = await pool.query("Select u.niveau, i.quantité, i.id_inventaire from utilisateur u join inventaire i on u.id_utilisateur = i.fk_utilisateur where i.fk_utilisateur =$1 and i.fk_ressource = 16", [id_utilisateur]);
+        //pour passer du niveau 1 au 2
+        if (resultat.rows[0].niveau == 1) {
+            if (resultat.rows[0].quantité >= 250) {
+                //retirer les kamas necessaire pour le passge de niveau
+                await pool.query("update inventaire set quantité=$1 where id_inventaire=$2", [resultat.rows[0].quantité - 250, resultat.rows[0].id_inventaire]);
+                await pool.query("update utilisateur set niveau=2 where id_utilisateur=$1", [id_utilisateur]);
+                return res.status(200).json({ message: "vous etes maintenant lvl 2" });
+            } else {
+                return res.status(400).json({ message: "pas assez de kamas" });
+            }
+        }
+        //pour passer du niveau 2 au 3
+        else {
+            if (resultat.rows[0].quantité >= 500) {
+                //retirer les kamas necessaire pour le passge de niveau
+                await pool.query("update inventaire set quantité=$1 where id_inventaire=$2", [resultat.rows[0].quantité - 500, resultat.rows[0].id_inventaire]);
+                await pool.query("update utilisateur set niveau=3 where id_utilisateur=$1", [id_utilisateur]);
+                return res.status(200).json({ message: "vous etes maintenant lvl 3" });
+            } else {
+                return res.status(400).json({ message: "pas assez de kamas" });
+            }
+        }
+    } catch (err) {
+        console.error(err)
+        res.status(500).send("Erreur agmenter niveau");
+    }
+});
+
+//afficher metiers
+app.get("/metiers", async(req, res) => {
+    try {
+        const resultat = await pool.query("Select nom, id_metier from metier");
+        return res.status(200).json({ message: "metiers récupéré avec succes", resultat: resultat.rows });
+    } catch (err) {
+        console.error(err)
+        res.status(500).send("Erreur metier");
+    }
+});
+
+//mettre en vente
+app.post("/mettre_vente", async(req, res) => {
+    const { id_utilisateur, id_inventaire, quantite, prix } = req.body;
+    try {
+        //
+        const resultat = await pool.query("select fk_ressource, quantité from inventaire where id_inventaire = $1", [id_inventaire]);
+        await pool.query("insert into vente(fk_utilisateur,fk_ressource, prix, quantite) values ($1,$2,$3,$4)", [id_utilisateur, resultat.rows[0].fk_ressource, prix, quantite]);
+        await pool.query("update inventaire set quantité= $1 where id_inventaire = $2", [resultat.rows[0].quantité - quantite, id_inventaire]);
+        return res.status(200).json({ message: "mise en vente" });
+    } catch (err) {
+        console.error(err)
+        res.status(500).send("Erreur metier");
+    }
+});
+
+//afficher le tableau des ventes
+app.get("/tableau_vente", async(req, res) => {
+    try {
+        const resultat = await pool.query("Select v.id_vente, v.prix, v.quantite, v.fk_utilisateur, v.fk_ressource, r.nom, u.pseudo from vente v join ressource r on v.fk_ressource = r.id_ressource join utilisateur u on u.id_utilisateur= v.fk_utilisateur ");
+        return res.status(200).json({ message: "tableau des vente récupéré avec succes", resultat: resultat.rows });
+    } catch (err) {
+        console.error(err)
+        res.status(500).send("Erreur tableau de vente");
+    }
+});
+
+//acheter un truc
+app.post("/acheter", async(req, res) => {
+    const { id_utilisateur, quantite, id_vente } = req.body;
+    try {
+        //recuperation kamas du joueur
+        const resultat = await pool.query("select quantité as kamas_possede from inventaire where fk_utilisateur = $1 and fk_ressource = 16", [id_utilisateur]);
+        //recuperer prix de l'article
+        const resultat2 = await pool.query("select prix, quantite, fk_utilisateur, fk_ressource from vente where id_vente = $1", [id_vente]);
+        //recuperer les kamas du joueur qui vend
+        const resultat3 = await pool.query("select quantité as kamas_vendeur from inventaire where fk_utilisateur=$1 and fk_ressource = 16", [resultat2.rows[0].fk_utilisateur]);
+        //recuperer la quantite de la ressource demande que possede le joueur qui achete
+        const resultat4 = await pool.query("select quantité as quantite_ressource from inventaire where fk_utilisateur =$1 and fk_ressource = $2", [id_utilisateur, resultat2.rows[0].fk_ressource]);
+        if (resultat.rows[0].kamas_possede > (resultat2.rows[0].prix * quantite)) {
+            //ajouter la somme a l'utilisateur qui a vendu
+            await pool.query("update inventaire set quantité=$1 where fk_utilisateur=$2 and fk_ressource = 16", [resultat3.rows[0].kamas_vendeur + resultat2.rows[0].prix * quantite, resultat2.rows[0].fk_utilisateur]);
+            //enlever la somme a l'utilisateur qui achete et lui ajouter ce qu'il a achete
+            await pool.query("update inventaire set quantité=$1 where fk_utilisateur=$2 and fk_ressource = 16", [resultat.rows[0].kamas_possede - resultat2.rows[0].prix * quantite, id_utilisateur]);
+            await pool.query("update inventaire set quantité=$1 where fk_utilisateur=$2 and fk_ressource = $3", [resultat4.rows[0].quantite_ressource + quantite, id_utilisateur, resultat2.rows[0].fk_ressource]);
+            //il achete tout le stock
+            if (quantite == resultat2.rows[0].quantite) {
+                //supprime la ligne vente
+                await pool.query("DELETE FROM vente WHERE id_vente=$1", [id_vente]);
+            } else {
+                //au met a jour la ligne avec des quantites en moins
+                await pool.query("Update vente set quantite= $1 WHERE id_vente=$2", [resultat2.rows[0].quantite - quantite, id_vente]);
+            }
+        }
+        return res.status(200).json({ message: "achat effectué" });
+    } catch (err) {
+        console.error(err)
+        res.status(500).send("Erreur achat");
+    }
+});
+
+//recuperer un objet qu'on a mis en vente
+app.post("/annuler_vente", async(req, res) => {
+    const { id_utilisateur, id_vente } = req.body;
+    try {
+        //on recupere la ressource et la quantite mise en vente
+        const resultat = await pool.query("Select fk_ressource, quantite from vente where id_vente =$1 and fk_utilisateur = $2", [id_vente, id_utilisateur]);
+        if (resultat.rows.length > 0) {
+            //recuperer la quantite que le joueur possede de cette ressource dans son inventaire
+            const resultat2 = await pool.query("Select id_inventaire ,quantité as quantite_ressource_possede from inventaire where fk_utilisateur= $1 and fk_ressource=$2", [id_utilisateur, resultat.rows[0].fk_ressource]);
+            //mettre a jour l'inventaire du joueur avec la nouvelle quantite
+            await pool.query("update inventaire set quantité=$1 where id_inventaire=$2", [resultat2.rows[0].quantite_ressource_possede + resultat.rows[0].quantite, resultat2.rows[0].id_inventaire]);
+            //enlever la vente
+            await pool.query("DELETE FROM vente WHERE id_vente=$1", [id_vente]);
+            return res.status(200).json({ message: "mise en vente annule", resultat: resultat.rows });
+        } else {
+            return res.status(40).json({ message: "vous ne pouvez pas enlever cette vente" });
+        }
+
+    } catch (err) {
+        console.error(err)
+        res.status(500).send("Erreur annulation mise en vente");
+    }
+});
+
+app.listen(port, () => {
+    console.log(`Serveur en écoute sur http://localhost:${port}`);
+});
